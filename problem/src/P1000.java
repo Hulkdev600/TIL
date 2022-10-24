2e77c14fc3f6bf66a5a44ce3b9cb44bbf893b1d0
이정훈 <58647036+Hulkdev600@users.noreply.github.com>
2022-10-25 05:59:57 +0900

    JPA기본_영속성컨텍스트 개념 정리

---
+++ problem/src/P1000.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P1000 {
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        int i = stdIn.nextInt();
+        int j = stdIn.nextInt();
+
+        int result = i + j;
+
+        System.out.println("result = " + result);
+
+    }
+}
---
+++ problem/src/P1001.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P1001 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int i = in.nextInt();
+        int j = in.nextInt();
+
+        System.out.println(i + j);
+
+        in.close();
+    }
+
+}
